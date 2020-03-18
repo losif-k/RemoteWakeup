b172b769c3367b7321df0f44ebe7b2d0b690ba65
losif-k <losifz248@gmail.com>
2020-03-19 00:55:32 +0900

    Modified main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import time
+powersw = 14
+resetsw = 15
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(powersw, GPIO.OUT)
+GPIO.setup(resetsw, GPIO.OUT)
+GPIO.output(powersw, GPIO.HIGH)
+GPIO.output(powersw, GPIO.HIGH)
+sw = input()
+if sw.isdigit() and int(sw) is (14 or 15):
+    if int(sw) is 14:
+        print("Shorted : 14")
+        GPIO.output(powersw, GPIO.LOW)
+        time.sleep(1)
+        GPIO.output(powersw, GPIO.HIGH)
+    if int(sw) is 15:
+        print("Shorted : 14")
+        GPIO.output(resetsw, GPIO.LOW)
+        time.sleep(1)
+        GPIO.output(resetsw, GPIO.HIGH)
