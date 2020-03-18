f4f4b0a071ed62379d0e13ac1de3b5cf4e58e6fa
losif-k <losifz248@gmail.com>
2020-03-19 00:56:56 +0900

    Modified main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(powersw, GPIO.OUT)
 GPIO.setup(resetsw, GPIO.OUT)
 GPIO.output(powersw, GPIO.HIGH)
-GPIO.output(powersw, GPIO.HIGH)
+GPIO.output(resetsw, GPIO.HIGH)
 sw = input()
 if sw.isdigit() and int(sw) is (14 or 15):
     if int(sw) is 14:
