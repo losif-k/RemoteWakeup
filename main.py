d68e6f8173fd8608ac5e534c461fc2d18b6353f3
losif-k <losifz248@gmail.com>
2020-03-19 01:25:43 +0900

    Modified main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import RPi.GPIO as GPIO
 import time
+from datetime import datetime
+import asyncio
+
 powersw = 14
 resetsw = 15
 
@@ -10,16 +13,42 @@ GPIO.setup(resetsw, GPIO.OUT)
 GPIO.output(powersw, GPIO.HIGH)
 GPIO.output(resetsw, GPIO.HIGH)
 
-while True:
-    sw = input()
-    if sw.isdigit() and int(sw) is (14 or 15):
-        if int(sw) is 14:
-            print("Shorted : 14")
-            GPIO.output(powersw, GPIO.LOW)
-            time.sleep(0.5)
+
+def autowakeup():
+    global powersw, resetsw
+    while True:
+        try:
+            date_time = datetime.now()
+            date_time.strftime("%H%M")
+            if date_time == "0900":
+                GPIO.output(powersw, GPIO.LOW)
+                time.sleep(0.5)
+                GPIO.output(powersw, GPIO.HIGH)
+            print(f"{'0900'} and {date_time} / {date_time == '0900'}")
+        except KeyboardInterrupt:
             GPIO.output(powersw, GPIO.HIGH)
-        if int(sw) is 15:
-            print("Shorted : 15")
-            GPIO.output(resetsw, GPIO.LOW)
-            time.sleep(0.5)
             GPIO.output(resetsw, GPIO.HIGH)
+            GPIO.cleanup()
+            return
+
+
+'''
+async def main():
+    while True:
+        try:
+            sw = input()
+            if sw.isdigit() and int(sw) is (14 or 15):
+                if int(sw) is 14:
+                    print("Shorted : 14")
+                    GPIO.output(powersw, GPIO.LOW)
+                    time.sleep(0.5)
+                    GPIO.output(powersw, GPIO.HIGH)
+                if int(sw) is 15:
+                    print("Shorted : 15")
+                    GPIO.output(resetsw, GPIO.LOW)
+                    time.sleep(0.5)
+                    GPIO.output(resetsw, GPIO.HIGH)
+
+        else:
+            continue
+'''
